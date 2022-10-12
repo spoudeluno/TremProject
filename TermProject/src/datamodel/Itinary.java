fe12d4fd4c1bff11d0f7a9921fde5c6a805a10bd
himatrek <himatrek@DESKTOP-LBLSU48>
2022-10-11 20:47:37 -0500

    Added Hello World Servelet

--- TermProject/src/datamodel/Itinary.java
+++ TermProject/src/datamodel/Itinary.java
@@ -1,5 +1,27 @@
 package datamodel;
 
 public class Itinary {
+	private int day;
+	private String Place;
+	private String Desc;
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public String getPlace() {
+		return Place;
+	}
+	public void setPlace(String place) {
+		Place = place;
+	}
+	public String getDesc() {
+		return Desc;
+	}
+	public void setDesc(String desc) {
+		Desc = desc;
+	}
+	
 
 }
