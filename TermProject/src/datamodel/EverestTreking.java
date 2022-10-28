c07cb5a1059ec7f0f5b0bb8e7abdfed7179b27c6
himatrek <himatrek@DESKTOP-LBLSU48>
2022-10-28 13:04:22 -0500

    Test

--- TermProject/src/datamodel/EverestTreking.java
+++ TermProject/src/datamodel/EverestTreking.java
@@ -1,14 +1,55 @@
 package datamodel;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table (name = "EverestTreking")
-public class EverestTreking extends Itinary{
-
+public class EverestTreking {
+	public EverestTreking() {
+			
+		}
 	public EverestTreking(String desc) {
-		super(desc);
+		this.desc = desc;
+	}
+
+	public EverestTreking(int day, String desc) {
+		this.day = day;
+		this.desc = desc;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "day")
+	private int day;
+	
+	@Column(name = "desc")
+	private String desc;
+	
+	public int getDay() {
+		return day;
+	}
+	
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+	
+	public String getDesc() {
+		return desc;
+	}
+	
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	@Override
+	public String toString() {
+		return "EverestTreking [day=" + day + ", desc=" + desc + "]";
 	}
 	
 
